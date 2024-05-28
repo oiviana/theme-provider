ed08b7f5d69e4687b122815a93a54cc412d44410
oiviana <vianalucas80@gmail.com>
2024-05-28 17:15:28 -0300

    config: project colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,17 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend:{
+      colors: {
+        'dark-300':'#1e293b',
+        'dark-500': '#0f172a',
+        'light-300': '#f2f2f2',
+        'light-500': '#ecedec',
+
+      },
+    }
+  },
   plugins: [],
   darkMode: 'class'
 };
