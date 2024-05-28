59921931d82b2f4aefd0322c45b8ebe82032889a
oiviana <vianalucas80@gmail.com>
2024-05-28 17:13:25 -0300

    test: calling theme switcher

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,9 +1,12 @@
+import ThemeSwitcher from "@/components/client/ThemeSwitcher";
+
 export default function Header(){
 
     return(
         <header>
             <div>
                 <h1>Next Themes Provider</h1>
+                <ThemeSwitcher/>
             </div>
         </header>
     )
