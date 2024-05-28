dae23223b5c27f9aa2aec8eb035eaac1eca7c2b0
oiviana <vianalucas80@gmail.com>
2024-05-28 15:55:17 -0300

    feat: header first structure

---
+++ src/components/layout/Header/index.tsx
@@ -0,0 +1,10 @@
+export default function Header(){
+
+    return(
+        <header>
+            <div>
+                <h1>Next Themes Provider</h1>
+            </div>
+        </header>
+    )
+}
