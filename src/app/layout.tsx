3294ead64cc21458ab4f1491e6e7494b1db609be
oiviana <vianalucas80@gmail.com>
2024-05-28 17:12:51 -0300

    refactor: provider structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./styles/globals.css";
-import Providers from "@/utils/Providers";
+import { Providers } from "@/utils/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/utils/Providers.tsx
+++ src/utils/Providers.tsx
@@ -1,19 +1,14 @@
-"use client"
+"use client";
 
-import React, { ReactNode } from "react";
-import { ThemeProvider } from "next-themes";
+import {NextUIProvider} from '@nextui-org/react'
+import {ThemeProvider as NextThemesProvider} from "next-themes";
 
-interface ProviderProps {
-    children: ReactNode;
-  }
-  
-
-const Providers: React.FC<ProviderProps> = ({ children }) => {
-    return(
-        <ThemeProvider attribute="class" defaultTheme="dark">
-            {children}
-        </ThemeProvider>
-    )
+export function Providers({children}: { children: React.ReactNode }) {
+  return (
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        {children}
+      </NextThemesProvider>
+    </NextUIProvider>
+  )
 }
-
-export default Providers;
