84d597555ea2eb8b2f19f18d9c03783427086317
oiviana <vianalucas80@gmail.com>
2024-05-29 00:13:17 -0300

    style: layout style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Header from "@/components/layout/Header";
 
 export default function Home() {
   return (
-  <>
+  <div className="w-full max-w-5xl mx-auto px-2 lg:px-0">
   <Header/>
-  </>
+  </div>
   );
 }
