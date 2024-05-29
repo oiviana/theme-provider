820d9e67098b17c0babde0f6ccf543bad7c47e7e
oiviana <vianalucas80@gmail.com>
2024-05-29 00:13:50 -0300

    style: header layout

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,13 +1,10 @@
 import ThemeSwitcher from "@/components/client/ThemeSwitcher";
 
-export default function Header(){
-
-    return(
-        <header>
-            <div>
-                <h1>Next Themes Provider</h1>
-                <ThemeSwitcher/>
-            </div>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header className="flex w-full justify-between py-6 items-center border-b-[1px] border-light-100 dark:border-dark-300">
+      <h1 className="font-bold italic text-lg lg:text-xl text-dark-500 dark:text-light-300">Next Themes Provider</h1>
+      <ThemeSwitcher />
+    </header>
+  );
 }
